4979385c1149a4cc905221790f3ed7359f6a8faa
manik005 <manik.005@gmail.com>
2026-02-14 19:56:48 +0530

    Change 'POS Next' to 'EasyPOS' in text

--- POS/src/pages/POSSale.vue
+++ POS/src/pages/POSSale.vue
@@ -416,7 +416,7 @@
 						</svg>
 					</div>
 					<h3 class="mt-4 text-lg font-medium text-gray-900">
-						{{ __("Welcome to POS Next") }}
+						{{ __("Welcome to EasyPOS") }}
 					</h3>
 					<p class="mt-2 text-sm text-gray-500">
 						{{ __("Please open a shift to start making sales") }}
@@ -770,7 +770,7 @@
 								{{ __("Sign Out?") }}
 							</h3>
 							<p class="text-sm text-gray-600">
-								{{ __("You will be logged out of POS Next") }}
+								{{ __("You will be logged out of EasyPOS") }}
 							</p>
 						</div>
 
